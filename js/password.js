9a962295ac5d76e02f241d7bf0c6fef7155230c0
baelinc <11509732+baelinc@users.noreply.github.com>
2024-08-04 19:37:32 -0500

    Update password.js

--- js/password.js
+++ js/password.js
@@ -7,15 +7,19 @@ document.addEventListener('DOMContentLoaded', () => {
     if (passwordForm && passwordInput && cancelButton) {
         passwordForm.addEventListener('submit', function(event) {
             event.preventDefault();
+            console.log('Form submitted');
             if (passwordInput.value === correctPassword) {
+                console.log('Password correct');
                 window.location.href = 'edit_data.html';
             } else {
+                console.log('Password incorrect');
                 alert('Incorrect password. Please try again.');
                 passwordInput.value = '';
             }
         });
 
         cancelButton.addEventListener('click', function() {
+            console.log('Cancel button clicked');
             window.location.href = 'index.html';
         });
     } else {
